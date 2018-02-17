e386f219214542f63a2c52970654ac4deb98d9f3
Connor Young <connorbubgames@gmail.com>
2018-02-17 14:19:05 -0500

    Added features
    Median works for any length list, added getOutliers() function

--- src/main/SensorSimulator.java
+++ src/main/SensorSimulator.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -18,12 +19,34 @@ public class SensorSimulator {
 		System.out.print(nineNumbers.get(nineNumbers.size()-1) + "\n");
 		System.out.println("\nThe Median is: " + getMedian(nineNumbers) + "\n");
 		System.out.println("The standard deviation is: " + getStandardDev(nineNumbers));
+		System.out.println("\nThe outliers (more than one std dev from the median) are:");
+		List<Integer> outliers = getOutliers(nineNumbers);
+		if (outliers.size() >= 1) {
+			for(int i=0; i<outliers.size() - 1; i++) {
+				System.out.print(outliers.get(i) + ",");
+			}
+			System.out.print(outliers.get(outliers.size()-1) + "\n");
+		} else {
+			System.out.println("No outliers in this set!");
+		}
 	}
 	
 	//Get median of integer list
-	public static int getMedian(List<Integer> nineNumbers) {
-		Collections.sort(nineNumbers);
-		int median = nineNumbers.get(4);
+	public static float getMedian(List<Integer> numbers) {
+		int midpoint = 0;
+		float median = 0;
+		//Sort the list
+		Collections.sort(numbers);
+		//If the list has an even amount of items
+		if (numbers.size() % 2 == 0) {
+			//Find midpoint of list and set median equal to the average of the middle 2 numbers
+			midpoint = (numbers.size())/2;
+			median = (numbers.get(midpoint) + numbers.get(midpoint - 1))/2;
+		} else {
+			//If list has odd number of items, simply find the midpoint and choose that value
+			midpoint = (numbers.size())/2;
+			median = numbers.get((int) (midpoint + 0.5));
+		}
 		return median;
 		
 	}
@@ -59,6 +82,20 @@ public class SensorSimulator {
 		return stdDev;
 	}
 	
+	//Find outliers from number list
+	public static List<Integer> getOutliers(List<Integer> list) {
+		List<Integer> outlierList = new ArrayList<Integer>();
+		float med = getMedian(list);
+		float stdDev = getStandardDev(list);
+		for (int i=0; i<list.size(); i++) {
+			if (list.get(i) < med - stdDev || list.get(i) > med + stdDev) {
+				outlierList.add(list.get(i));
+			}
+		}
+		return outlierList;
+	}
+	
+	
 	//Generate a list of 9 random numbers
 	public static List<Integer> generateNineNumbers(Random rand) {
 		List<Integer> list = new ArrayList<Integer>();
@@ -68,6 +105,4 @@ public class SensorSimulator {
 		}
 		return list;
 	}
-	
-	
 }
